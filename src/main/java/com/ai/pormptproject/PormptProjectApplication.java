937a17b151202ea3b0823edcd8724e8ffe0932af
zf670471 <zf670471876@protonmail.com>
2023-05-07 23:27:07 +0800

    'first'

---
+++ src/main/java/com/ai/pormptproject/PormptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ai.pormptproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PormptProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PormptProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/pormptproject/PormptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.pormptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PormptProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
